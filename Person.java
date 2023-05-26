36ac46e696c4ff83a2acdd2b46b5848715978bbb
OlhaSyd <karapylja@gmail.com>
2023-05-26 21:09:54 +0300

    Homework #13 fixed

--- Person.java
+++ Person.java
@@ -1,32 +1,30 @@
 package com.sydorenko;
 
 public class Person {
+
     public String name;
     public String city;
     public String telefone;
 
+    public Person(String personName, String personCity, String personTelefone) {
+        this.name = personName;
+        this.city = personCity;
+        this.telefone = personTelefone;
+
+    }
+
     public static void main(String[] args) {
-        Person person1 = new Person();
-        person1.name = "Will Smith";
-        person1.city = "New York";
-        person1.telefone = "2936729462846";
+        Person person1 = new Person("Will Smith", "New York", "2936729462846");
 
-        Person person2 = new Person();
-        person2.name = " Jackie Chan";
-        person2.city = "Shanghai ";
-        person2.telefone = "12312412412";
+        Person person2 = new Person("Jackie Chan", "Shanghai ", "12312412412");
 
-        Person person3 = new Person();
-        person3.name = "Sherlock Holmes ";
-        person3.city = " London";
-        person3.telefone = "37742123513";
+        Person person3 = new Person("Sherlock Holmes ", " London", "37742123513");
 
         person1.personInfo();
         System.out.println();
 
         person2.personInfo();
         System.out.println();
-
         person3.personInfo();
     }
 
